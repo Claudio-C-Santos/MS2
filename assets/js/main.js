6e6dda402f01ca3e94121f0d990b98143e3e3d02
Claudio-C-Santos <cula.santos@gmail.com>
2020-12-05 15:03:34 +0000

    <build>image, question and quote section built and styled

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
-$("button").on("click", function() {
-    $("button").removeClass("btn");
-});
+$(document).on("ready", ajaxCall());
 
+function ajaxCall() {
+  $.ajax({
+    method: 'get',
+    url: 'https://thesimpsonsquoteapi.glitch.me/quotes'
+  })
+    .done(function(data) {
+      $('.quote').html(`"${data[0].quote}"`);
+      $('.author').html(`&mdash; ${data[0].character}`);
+      $('.character').attr('src', data[0].image);
+    })
+    .fail(function() {
+      alert('Something went wrong.');
+    });
+}
