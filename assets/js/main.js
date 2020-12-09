101d401245f0773fda4c1e084c069952abdb330e
Claudio-C-Santos <cula.santos@gmail.com>
2020-12-09 19:19:18 +0000

    <build>Introduction screen built

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,100 @@
-$(document).on("ready", ajaxCall());
+let questionBlock = document.querySelector("#block-question");
+let levelBlock = document.querySelector("#level-block");
+let gameBlock = document.querySelector("#gameBlock");
 
+
+let score = 0;
+let level = 1;
+
+let questionDuration = 15;
+let secondsElapsed = 0;
+let secondsIncrement;
+
+
+//Call API Simpsons' quotes
+ajaxCall();
+$(document).ready(function(){
+    let fullWidth = $("#gameBlock").width();
+    let levelBlock = $("#level-block").width();
+    $("#block-question").width(fullWidth - levelBlock);
+})
+
+init();
+
+// This function outputs the introduction screen with a button to start the game.
+function init() {
+    clearBlock();
+    reset();
+
+    let heading = document.createElement("h1");
+    heading.setAttribute("class", "simpsonsFont");
+    heading.setAttribute("id", "introTitle");
+    heading.textContent = "Welcome to The Simpsons Quiz!";
+    questionBlock.appendChild(heading);
+
+    let description = document.createElement("p");
+    description.setAttribute("class", "descrText");
+    description.textContent = `This quiz will test your knowledge about the amazing TV show THE SIMPSONS. \r\n 
+    There are 20 levels to go through, each of them is based of different aspects of the show. \r\n 
+    You will have 15 seconds to answer each question and once the answer is answered correctly, the remaining seconds will be added as points to your score. There are now second chances so let's see if you are a real Simpsons Fan!\r\n
+    Whenever you are ready just press the button below to start the game!`;
+    questionBlock.appendChild(description);
+
+    let startButton = document.createElement("button");
+    startButton.setAttribute("class", "btn");
+    startButton.textContent = `Ay Caramba!`;
+    questionBlock.appendChild(startButton);
+
+    startButton.addEventListener("click", function() {
+        startQuiz();
+        displayLevel();
+    })
+}
+
+//This function clears all elements children of the main block
+function clearBlock() {
+    questionBlock.innerHTML="";
+}
+
+//This function resets all counts
+function reset() {
+    score = 0;
+    level = 1;
+
+    questionDuration = 15;
+    secondsElapsed = 0;
+    secondsIncrement;
+}
+
+//This function starts the game by displaying the question and answer options
+function startQuiz() {
+    clearBlock(); 
+
+
+
+
+
+}
+
+//This function will display a block on the left of the screen with the current level. The current level will be displayed with an image (Images Source: https://fanart.tv/series/71663/the-simpsons/).
+function displayLevel() {
+    let levelBlock = document.createElement("div");
+    levelBlock.setAttribute("class", "block");
+    levelBlock.setAttribute("id", "level-block");
+    gameBlock.insertBefore(levelBlock, gameBlock.firstChild);
+
+    questionBlock.setAttribute("style", "margin-left: 30px;");
+
+    let levelImage = document.createElement("img");
+    levelImage.setAttribute("id", "level-img");
+    levelImage.setAttribute("alt", "level-image");
+    levelImage.setAttribute("src", "assets/images/1.jpg");
+    levelBlock.appendChild(levelImage);
+}
+
+
+//API with The Simpsons random quotes and their author.
+// https://github.com/jaridwarren/simpsons-quotes
 function ajaxCall() {
   $.ajax({
     method: 'get',
---
+++ assets/js/questions.js
@@ -0,0 +1,109 @@
+//General questions
+let questionsLevel1 = [
+    {
+        question: "What color is everyone's skin in The Simpson's world?",
+        options: ["blue", "green", "yellow", "pink"],
+        answer: "yellow"
+    },
+    {
+        question: "How many kids are there in The Simpson's family?",
+        options: ["4", "5", "7", "3"],
+        answer: "3"
+    }
+]
+
+//Questions about the family member's names
+let questionsLevel2 = [
+    {
+        question: "What is the father's name in The Simpson's family?",
+        options: ["Mr. Burns", "Bart", "Homer", "Carl"],
+        answer: "Homer"
+    },
+    {
+        question: "What is the mother's name in The Simpson's family?",
+        options: ["Lisa", "Edna", "Patty", "Marge"],
+        answer: "Marge"
+    },
+    {
+        question: "What is the son's name in The Simpson's family?",
+        options: ["Bart", "Ned", "Moe", "Apu"],
+        answer: "Bart"
+    },
+    {
+        question: "What is the older daughter's name in The Simpson's family?",
+        options: ["Lisa", "Maude", "Selma", "Marge"],
+        answer: "Lisa"
+    },
+    {
+        question: "What is the baby's name in The Simpson's family?",
+        options: ["Lisa", "Patty", "Maggie", "Marge"],
+        answer: "Maggie"
+    }
+]
+
+//Questions about Homer
+let questionsLevel3 = [
+    {
+        question: "What is Homer's favourite beer?",
+        options: ["Bud", "Pawtucket", "Heineken", "Duff"],
+        answer: "Duff"
+    },
+    {
+        question: "What is the color of Homer's shirt?",
+        options: ["Orange", "White", "Green", "Black"],
+        answer: "White"
+    },
+    {
+        question: "What is the color of Homer's pants?",
+        options: ["Blue", "White", "Green", "Black"],
+        answer: "Blue"
+    },
+    {
+        question: "What is the name of Homer's wife?",
+        options: ["Lisa", "Maude", "Selma", "Marge"],
+        answer: "Marge"
+    }
+]
+
+//Questions about Lisa
+let questionsLevel4 = [
+    {
+        question: "What is Lisa's favourite musical instrument?",
+        options: ["Saxophone", "Trumpet", "Bandolin", "Guitar"],
+        answer: "Saxophone"
+    },
+    {
+        question: "What is the color of Lisa's dress?",
+        options: ["Orange", "White", "Green", "Black"],
+        answer: "Orange"
+    },
+    {
+        question: "Lisa is ...",
+        options: ["Older than Homer", "Younger than Maggie", "Older than Marge", "Older than Bart"],
+        answer: "Younger than Bart"
+    },
+    {
+        question: "What is the name of Lisa's brother?",
+        options: ["Apu", "Ralph", "Bart", "Milhouse"],
+        answer: "Bart"
+    }
+]
+
+//Questions about Bart
+let questionsLevel5 = [
+    {
+        question: "What is the color of Bart's shorts?",
+        options: ["White", "Yellow", "Blue", "Green"],
+        answer: "Blue"
+    },
+    {
+        question: "Who is Bart's best friend?",
+        options: ["Homer", "Ralph", "Carl", "Milhouse"],
+        answer: "Milhouse"
+    },
+    {
+        question: "What is the name of Bart's father?",
+        options: ["Homer", "Carl", "Moe", "Skinner"],
+        answer: "Homer"
+    },
+]
