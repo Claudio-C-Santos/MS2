d06dddf71e8ec93dea97b92264c9254335a47b4e
Claudio-C-Santos <cula.santos@gmail.com>
2020-12-14 18:36:34 +0000

    <build>List of questions finished

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,14 +1,19 @@
 //General questions
 let questionsLevel1 = [
     {
-        question: "Which is the main color of The Simpsons world?",
+        question: "Which is the main color of the Simpsons world?",
         options: ["blue", "green", "yellow", "pink"],
         answer: "yellow"
     },
     {
-        question: "How many kids are there in The Simpsons family?",
+        question: "How many kids are there in the Simpsons family?",
         options: ["Four", "Five", "Seven", "Three"],
         answer: "Three"
+    },
+    {
+        question: "What is the name of the simpsons' neighbour?",
+        options: ["Milhouse", "Carl", "Moe", "Flanders"],
+        answer: "Flanders"
     }
 ]
 
@@ -106,4 +111,315 @@ let questionsLevel5 = [
         options: ["Homer", "Carl", "Moe", "Skinner"],
         answer: "Homer"
     },
+]
+
+//Questions about Marge
+let questionsLevel6 = [
+    {
+        question: "What is the color of Marge's hair?",
+        options: ["White", "Yellow", "Blue", "Green"],
+        answer: "Blue"
+    },
+    {
+        question: "Which are the names of Marge's children?",
+        options: ["Maggie, Lisa and Mr. Burns", "Bart, Lisa and Maggie", "Bart, Lisa and Moe", "Milhouse, Bart and Lisa"],
+        answer: "Bart, Lisa and Maggie"
+    },
+    {
+        question: "Is Homer Marge's husband?",
+        options: ["Yes", "No"],
+        answer: "Yes"
+    },
+]
+
+//Questions about Maggie
+let questionsLevel7 = [
+    {
+        question: "Has Maggie learned how to speak?",
+        options: ["Yes", "No"],
+        answer: "No"
+    },
+    {
+        question: "Which are the name of Maggie's parents?",
+        options: ["Homer and Marge", "Homer and Selma", "Carl and Marge", "Moe and Marge"],
+        answer: "Homer and Marge"
+    },
+    {
+        question: "Which other character has the same hair shape as Maggie?",
+        options: ["Milhouse", "Lisa", "Homer", "Marge"],
+        answer: "Lisa"
+    },
+]
+
+//Questions related to Bart's friends 
+let questionsLevel8 = [
+    {
+        question: "Is Milhouse considered Bart's best friend?",
+        options: ["Yes", "No"],
+        answer: "Yes"
+    },
+    {
+        question: "Who is Bart's bully in the show?",
+        options: ["Homer", "Nelson", "Milhouse", "Moe"],
+        answer: "Nelson"
+    },
+    {
+        question: "Which of the following is Bart's nemesis?",
+        options: ["Duffman", "Sideshow Mel", "Krusty The Clown", "Sideshow Bob"],
+        answer: "Sideshow Bob"
+    },
+    {
+        question: "Does Milhouse were glasses?",
+        options: ["No", "Yes"],
+        answer: "Yes"
+    },
+    {
+        question: "Does Nelson were glasses?",
+        options: ["No", "Yes"],
+        answer: "No"
+    },
+]
+
+//Questions about Flanders
+let questionsLevel9 = [
+    {
+        question: "Is Flanders a Evangelical Christian?",
+        options: ["Yes", "No"],
+        answer: "Yes"
+    },
+    {
+        question: "Does Flanders have a mustache?",
+        options: ["Yes", "No"],
+        answer: "Yes"
+    },
+    {
+        question: "How many kids does Flanders have in The Simpsons?",
+        options: ["Two", "Three", "Six", "One"],
+        answer: "Two"
+    },
+]
+
+//Questions about characters related to Moe's bar
+let questionsLevel10 = [
+    {
+        question: "What is the name of Homer's favourite bar?",
+        options: ["Drunken Clam", "Moe's", "Cheers","Oneil's"],
+        answer: "Moe's"
+    },
+    {
+        question: "What is the name of the owner of Homer's favourite bar?",
+        options: ["Carl", "Barney", "Lenny", "Moe"],
+        answer: "Moe"
+    },
+    {
+        question: "What does Homer usually drink at Moe's?",
+        options: ["Vodka", "Beer", "Water", "Wine"],
+        answer: "Beer"
+    },
+    {
+        question: "With who does Homer usually spend his time at Moe's?",
+        options: ["Lisa and Bart", "Lenny and Carl", "Moe and Lenny", "Carl and Moe"],
+        answer: "Lenny and Carl"
+    },
+]
+
+//Questions about Krusty
+let questionsLevel11 = [
+    {
+        question: "Who is Krusty?",
+        options: ["The Clown", "The SideShow Krusty", "The Principal","The Comic Book Guy"],
+        answer: "The Clown"
+    },
+    {
+        question: "Does Krusty have a TV Show?",
+        options: ["Yes", "No"],
+        answer: "Yes"
+    }
+]
+
+//Questions about Comic Book Guy
+let questionsLevel12 = [
+    {
+        question: "The Comic Book Guy sells two items in his store, which are they?",
+        options: ["Gum, baseball cards and comic books", "Baseball Cards and gum", "Comic books and baseball cards","Comic books and gum"],
+        answer: "Comic books and baseball cards"
+    },
+    {
+        question: "Which of these describes the Comic Book Guy's physical appearance?",
+        options: ["Obese", "Slim", "Muscular", "Crippled"],
+        answer: "Obese"
+    },
+    {
+        question: "Is the Comic Book Guy considered a geek?",
+        options: ["Yes", "No"],
+        answer: "Yes"
+    }
+]
+
+//Questions about Ralph Wiggum
+let questionsLevel13 = [
+    {
+        question: "Who is Ralph Wiggum's father?",
+        options: ["Homer Wiggum", "Chief Wiggum", "Kent Wiggum", "Groundskeeper Wiggum"],
+        answer: "Chief Wiggum"
+    },
+    {
+        question: "What is Raplh Wiggum always picking?",
+        options: ["His elbow", "His hear", "His nose", "His neck"],
+        answer: "His nose"
+    },
+    {
+        question: "Is Ralph Wiggum considered smart?",
+        options: ["Yes", "No"],
+        answer: "No"
+    }
+]
+
+//Questions about Aliens, Kang and Kodos
+let questionsLevel14 = [
+    {
+        question: "Are there aliens the The Simpsons?",
+        options: ["Yes", "No"],
+        answer: "Yes"
+    },
+    {
+        question: "What are the aliens' color the The Simpson?",
+        options: ["Orange", "Black", "Blue", "Green"],
+        answer: "Green"
+    }
+]
+
+//Questions about Bart and Lisa's school
+let questionsLevel15 = [
+    {
+        question: "What is the name of Bart's school?",
+        options: ["Quahog Elementary School", "Springfield Elementary School", "Cleveland Elementary School","New York Elementary School"],
+        answer: "Springfield Elementary School"
+    },
+    {
+        question: "What is the name of Springfield Elementary School?",
+        options: ["Ned Flanders", "Nelson Muntz", "Fat Tony", "Seymour Skinner"],
+        answer: "Seymour Skinner"
+    },
+    {
+        question: "Which of these characters are seen with Bart in school?",
+        options: ["Homer and Carl", "Milhouse and Nelson", "Mr. Burns and Smithers", "Lisa and Maggie"],
+        answer: "Milhouse and Nelson"
+    }
+]
+
+//Questions about other characters
+let questionsLevel16 = [
+    {
+        question: "What does Mr. Burns own?",
+        options: ["Nuclear Power Plant", "Kwik-E-Mart", "Comic Book Store", "Bar"],
+        answer: "Nuclear Factory"
+    },
+    {
+        question: "Who owns Kwik-E-Mart?",
+        options: ["Flanders", "Apu", "Homer", "Moe"],
+        answer: "Apu"
+    },
+    {
+        question: "Where is Groundskeeper Willie from?",
+        options: ["Ireland", "Portugal", "Netherlands", "Germany"],
+        answer: "Ireland"
+    },
+    {
+        question: "What is the name of the character that has a bone stuck in his hair?",
+        options: ["SideShow Bob", "SideShow Mel", "Kent Brockman", "Mayor Quimby"],
+        answer: "SideShow Mel"
+    },
+    {
+        question: "Who is Kent Brockman?",
+        options: ["TV host", "Priest", "Mayor", "Doctor"],
+        answer: "TV host"
+    }
+]
+
+//Questions about the show's generic
+let questionsLevel17 = [
+    {
+        question: "Where doe the Simpson sit at the end of every generic?",
+        options: ["On the floor", "On their couch", "On the stairs", "In separate rooms"],
+        answer: "On their couch"
+    },
+    {
+        question: "In the generic Homer is ran over by who?",
+        options: ["Marge", "Bart", "Lisa", "Maggie"],
+        answer: "Marge"
+    },
+    {
+        question: "Is the generic the same on very The Simpsons episode?",
+        options: ["Yes", "No"],
+        answer: "No"
+    },
+    {
+        question: "Who is in detention at the beginning of every episode?",
+        options: ["Marge", "Bart", "Lisa", "Maggie"],
+        answer: "Bart"
+    }
+]
+
+//Questions about each character's job
+let questionsLevel18 = [
+    {
+        question: "Where does Homer work?",
+        options: ["Nuclear Power Plant", "Moe's", "Kwik-E-Mart", "Hospital"],
+        answer: "Nuclear Power Plant"
+    },
+    {
+        question: "Who is Springfield's Police Chief?",
+        options: ["Chief Raplh", "Chief Flanders", "Chief Homer", "Chief Wiggum"],
+        answer: "Chief Wiggum"
+    },
+    {
+        question: "What is the name of Springfield's Mayor?",
+        options: ["Quimby", "Ralph", "Wiggum", "Brockman"],
+        answer: "Quimby"
+    },
+    {
+        question: "What is the name of Springfield's News anchor?",
+        options: ["Ned Flanders", "Ralph Wiggum", "Kent Brockman", "Maggie Simpson"],
+        answer: "Kent Brockman"
+    }
+]
+
+//Questions about characters famous quotes
+let questionsLevel19 = [
+    {
+        question: "Who says...`D'oh!`?",
+        options: ["Homer Simpson", "Bart Simpson", "Maggie Simpson", "Marge Simpson"],
+        answer: "Homer Simpson"
+    },
+    {
+        question: "Who says...`Ay Caramba!`?",
+        options: ["Homer Simpson", "Bart Simpson", "Maggie Simpson", "Marge Simpson"],
+        answer: "Bart Simpson"
+    },
+    {
+        question: "Who says...`Stupid Flanders!`?",
+        options: ["Homer Simpson", "Bart Simpson", "Maggie Simpson", "Marge Simpson"],
+        answer: "Homer Simpson"
+    },
+    {
+        question: "Who says...`It tastes like…burning!`?",
+        options: ["Nelson Muntz", "Bart Simpson", "Milhouse Van Houten", "Ralph Wiggum"],
+        answer: "Ralph Wiggum"
+    },
+    {
+        question: "Who says...`Ha Ha!`?",
+        options: ["Nelson Muntz", "Bart Simpson", "Milhouse Van Houten", "Ralph Wiggum"],
+        answer: "Nelson Muntz"
+    },
+
+]
+
+//Questions about characters famous quotes
+let questionsLevel20 = [
+    {
+        question: "Which year was the first episode of The Simpson broadcast?",
+        options: ["1989", "1945", "2011", "1992"],
+        answer: "1989"
+    }
 ]
